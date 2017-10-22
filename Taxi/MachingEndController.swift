fa6015b7c51e8f358a5bb83f5178a30cae0c5ec7
Katsuya Taguchi <nymph.y.s.0310@ezweb.ne.jp>
2017-10-22 17:57:37 +0900

    各画面のコントローラー作成

---
+++ Taxi/MachingEndController.swift
@@ -0,0 +1,31 @@
+//
+//  MachingEndController.swift
+//  Taxi
+//
+//  Created by Katsuya Taguchi on 2017/10/22.
+//  Copyright © 2017年 清野隼史. All rights reserved.
+//
+
+import UIKit
+
+class MachingEndController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var IDForm: UITextField!
+    
+    @IBOutlet weak var PasswordForm: UITextField!
+    
+    
+    @IBAction func SignInButton(_ sender: UIButton) {
+        
+    }
+}

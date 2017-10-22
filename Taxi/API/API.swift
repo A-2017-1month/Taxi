d80efc6954fac55ed5c61461137bcbbcbd46eb14
getty104 <hayabusatoshihumi@gmail.com>
2017-10-22 16:49:01 +0900

    update

--- Taxi/API/API.swift
+++ Taxi/API/API.swift
@@ -5,7 +5,9 @@
 //  Created by 清野隼史 on 2017/10/22.
 //  Copyright © 2017年 清野隼史. All rights reserved.
 //
-
+import Gryphon
 final class API {
-
+ static var baseURL: String {
+  return "ここにAPIのURLを入れる"
+}
 }
---
+++ Taxi/API/Users.swift
@@ -0,0 +1,46 @@
+import Alamofire
+import Gryphon
+
+extension API {
+  final class Users: Requestable {
+
+    enum Router: String {
+      case all = ""
+      case create = "/new"
+      case find = "/new/"
+    }
+
+    // required `Requestable`
+    static var path: String {
+      return baseURL + "/users" + router.rawValue
+    }
+
+    static var router: Router = .all // Default routing
+
+    // Returns message(String) from server or error reason(Error).
+    class func all() -> Task<[[String]], Error> {
+      router = .all
+      let task = Task<[[String]], Error> { result in
+
+        Alamofire.request(path, method: .get, encoding: JSONEncoding.default)
+        .responseJSON(completionHandler: { response in
+
+          // Object mapping in your favorite way.
+          guard let message = (response.result.value as! [AnyObject])[0]["result"] as? String else {
+
+            let reason = response.result.description as AnyObject
+
+            return result(APIResult<String>.error(ResponseError.unexceptedResponse(reason)))
+
+          }
+
+          result(APIResult<String>.response(message))
+
+          })
+
+      }
+
+      return task
+    }
+
+  }

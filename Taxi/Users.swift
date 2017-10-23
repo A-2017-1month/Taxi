1fef01a5082c2bba964946ea8845d4cd7ae9ea27
getty104 <hayabusatoshihumi@gmail.com>
2017-10-23 14:49:25 +0900

    upate

--- Taxi/Users.swift
+++ Taxi/Users.swift
@@ -10,10 +10,11 @@ extension API {
   }
 
   enum Router: String {
-    case create = "/new/"
-    case find = "/"
-    case update = "/update/"
+    case create =    "/new/"
+    case find =      "/"
+    case update =    "/update/"
     case get_match = "/get_match/"
+    case login =     "/login/"
   }
 
   static var router: Router = .find // Default routing
@@ -41,13 +42,16 @@ extension API {
     return task
   }
 
-  class func get_match(name: String, email: String, account_name: String) -> Task <Int, Error> {
-    router = .get_match
+  class func create(name: String, email: String, account_name: String, password: String, billing_number: String, billing_name: String) -> Task <Int, Error> {
+    router = .create
     value = ""
     let params : Parameters = [
     "name": name,
     "email": email,
-    "account_name": account_name
+    "account_name": account_name,
+    "password": password,
+    "billing_number": billing_number,
+    "billing_name": billing_name
     ]
 
     let task = Task<Int, Error> { result in
@@ -55,13 +59,30 @@ extension API {
       Alamofire.request(path, method: .post, parameters: params, encoding: JSONEncoding.default)
       .responseJSON(completionHandler: { response in
         guard let statusCode = response.response?.statusCode else { return }
-
         if 200...299 ~= statusCode {
           result(APIResult<Int>.response(statusCode))
           }else{
             result(APIResult<Int>.error(ResponseError.unacceptableStatusCode(statusCode)))
           }
           })
+    }
+    return task
+  }
+
+  class func login(account_name: String, password: String) -> Task <String, Error> {
+    router = .login
+    value = ""
+    let params : Parameters = [
+    "password": password,
+    "account_name": account_name
+    ]
+
+    let task = Task<String, Error> { result in
+      Alamofire.request(path, method: .get, encoding: JSONEncoding.default)
+      .responseJSON(completionHandler: { response in
+        let user = response.result.value as! [String : AnyObject]
+        result(APIResult<String>.response(user["auth"]))
+        })
     }
     return task
   }

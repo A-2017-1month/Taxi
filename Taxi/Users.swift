805f2a76a52bac8d687ecc24ec1b7016518c5310
getty104 <hayabusatoshihumi@gmail.com>
2017-10-23 12:43:47 +0900

    update

--- Taxi/Users.swift
+++ Taxi/Users.swift
@@ -18,17 +18,17 @@ extension API {
 
   static var router: Router = .find // Default routing
 
-  static var param: String = ""
+  static var value: String = ""
 
   // required `Requestable`
   static var path: String {
-    return baseURL + "/users" + router.rawValue + param
+    return baseURL + "/users" + router.rawValue + value
   }
 
   // Returns message(String) from server or error reason(Error).
   class func find(id: String) -> Task <[String : AnyObject], Error> {
     router = .find
-    param = id
+    value = id
 
     let task = Task<[String : AnyObject], Error> { result in
 
@@ -37,6 +37,31 @@ extension API {
         let user = response.result.value as! [String : AnyObject]
         result(APIResult< [String : AnyObject]>.response(user))
         })
+    }
+    return task
+  }
+
+  class func get_match(name: String, email: String, account_name: String) -> Task <Int, Error> {
+    router = .get_match
+    value = ""
+    let params : Parameters = [
+    "name": name,
+    "email": email,
+    "account_name": account_name
+    ]
+
+    let task = Task<Int, Error> { result in
+
+      Alamofire.request(path, method: .post, parameters: params, encoding: JSONEncoding.default)
+      .responseJSON(completionHandler: { response in
+        guard let statusCode = response.response?.statusCode else { return }
+
+        if 200...299 ~= statusCode {
+          result(APIResult<Int>.response(statusCode))
+          }else{
+            result(APIResult<Int>.error(ResponseError.unacceptableStatusCode(statusCode)))
+          }
+          })
     }
     return task
   }

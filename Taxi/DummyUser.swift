0f078927030ac8fa432f6b2e0ceaebf8f7b8ec0c
getty104 <hayabusatoshihumi@gmail.com>
2017-10-23 23:33:55 +0900

    udpdate

---
+++ Taxi/DummyUser.swift
@@ -0,0 +1,23 @@
+class DummyUser {
+  static var users : [[String : String]] = [
+  ["name": "田中一郎",
+  "email": "tanaka@ate.tm",
+  "gender": "男",
+  "password": "hoge",
+  "account_name": "hoge",
+  "different_ok?": "true",
+  "comment": "早く帰りたいです",
+  "longitude": "136.881537",
+  "latitude": "35.170915"],
+
+  ["name": "木村和子",
+  "email": "kimura@ate.tm",
+  "gender": "女",
+  "password": "hoge",
+  "account_name": "hoge",
+  "different_ok?": "true",
+  "comment": "少しの時間なら待てます",
+  "longitude": "136.881537",
+  "latitude": "35.170915"]
+  ]
+}
--- Taxi/MapController.swift
+++ Taxi/MapController.swift
@@ -10,17 +10,17 @@ import UIKit
 import MapKit
 
 class MapController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func editProfile(_ sender: UIButton) {
     }
     @IBOutlet weak var mapView: MKMapView!

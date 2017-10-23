f36aaac6986d767d13de32233587adf5b00571a0
getty104 <hayabusatoshihumi@gmail.com>
2017-10-23 20:35:28 +0900

    update

--- Taxi/MapController.swift
+++ Taxi/MapController.swift
@@ -11,47 +11,78 @@ import MapKit
 import CoreLocation
 
 class MapController: UIViewController {
+  var mylocationManager: CLLocationManager!
+  let ud = UserDefaults.standard
 
-    let ud = UserDefaults.standard
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    mylocationManager = CLLocationManager() // インスタンスの生成
+    mylocationManager.delegate = self  as! CLLocationManagerDelegate// CLLocationManagerDelegateプロトコルを実装するクラスを指定する
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+}
 
-    @IBAction func editProfile(_ sender: UIButton) {
-    }
-    @IBOutlet weak var mapView: MKMapView!
-    @IBOutlet weak var rideLocation: UITextField!
-    @IBAction func selectTime(_ sender: UIDatePicker) {
-    }
-    @IBAction func customEnd(_ sender: UIButton) {
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+}
 
-        let user1 : [String : String] = [
-        "name":"田中一郎",
-        "email": "tanaka@ate.tm",
-        "gender": "男",
-        "password": "hoge",
-        "account_name": "hoge",
-        "different_ok?": "true",
-        "comment": "早く帰りたいです"
-        ]
+@IBAction func editProfile(_ sender: UIButton) {
+}
+@IBOutlet weak var mapView: MKMapView!
+@IBOutlet weak var rideLocation: UITextField!
+@IBAction func selectTime(_ sender: UIDatePicker) {
+}
+@IBAction func customEnd(_ sender: UIButton) {
 
-        let user2 : [String : String] = [
-        "name":"木村和子",
-        "email": "kimura@ate.tm",
-        "gender": "女",
-        "password": "hoge",
-        "account_name": "hoge",
-        "different_ok?": "true",
-        "comment": "少しの時間なら待てます"
-        ]
+    let user1 : [String : String] = [
+    "name":"田中一郎",
+    "email": "tanaka@ate.tm",
+    "gender": "男",
+    "password": "hoge",
+    "account_name": "hoge",
+    "different_ok?": "true",
+    "comment": "早く帰りたいです"
+    ]
 
-        ud.set(user1, forKey: "user1Key")
-        ud.set(user2, forKey: "user2Key")
+    let user2 : [String : String] = [
+    "name":"木村和子",
+    "email": "kimura@ate.tm",
+    "gender": "女",
+    "password": "hoge",
+    "account_name": "hoge",
+    "different_ok?": "true",
+    "comment": "少しの時間なら待てます"
+    ]
+
+    ud.set(user1, forKey: "user1Key")
+    ud.set(user2, forKey: "user2Key")
+}
+}
+
+extension MapController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+            case .notDetermined:
+            print("ユーザーはこのアプリケーションに関してまだ選択を行っていません")
+            // 許可を求めるコードを記述する（後述）
+            mylocationManager.requestWhenInUseAuthorization()
+            break
+            case .denied:
+            print("ローケーションサービスの設定が「無効」になっています (ユーザーによって、明示的に拒否されています）")
+            // 「設定 > プライバシー > 位置情報サービス で、位置情報サービスの利用を許可して下さい」を表示する
+            break
+            case .restricted:
+            print("このアプリケーションは位置情報サービスを使用できません(ユーザによって拒否されたわけではありません)")
+            // 「このアプリは、位置情報を取得できないために、正常に動作できません」を表示する
+            break
+            case .authorizedAlways:
+            print("常時、位置情報の取得が許可されています。")
+            // 位置情報取得の開始処理
+            break
+            case .authorizedWhenInUse:
+            print("起動時のみ、位置情報の取得が許可されています。")
+            // 位置情報取得の開始処理
+            break
+        }
     }
 }

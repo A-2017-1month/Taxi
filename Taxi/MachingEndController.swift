987a9107aabcfaecca83c80e4d94b650fcc7d56b
getty104 <hayabusatoshihumi@gmail.com>
2017-10-23 23:45:51 +0900

    update

--- Taxi/MachingEndController.swift
+++ Taxi/MachingEndController.swift
@@ -10,17 +10,17 @@ import UIKit
 import MapKit
 
 class MachingEndController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBOutlet weak var matchingUserName: UILabel!
     @IBOutlet weak var mapView: MKMapView!
 }

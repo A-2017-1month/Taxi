6a8d081c79675fbf28bad3df72db4757c11b2c94
getty104 <hayabusatoshihumi@gmail.com>
2017-10-24 00:09:07 +0900

    update

--- Taxi/DummyUser.swift
+++ Taxi/DummyUser.swift
@@ -7,8 +7,8 @@ class DummyUser {
   "account_name": "hoge",
   "different_ok?": "true",
   "comment": "早く帰りたいです",
-  "longitude": "136.881537",
-  "latitude": "35.170915"],
+  "longitude": "136.884556",
+  "latitude": "35.17213"],
 
   ["name": "木村和子",
   "email": "kimura@ate.tm",
@@ -17,7 +17,7 @@ class DummyUser {
   "account_name": "hoge",
   "different_ok?": "true",
   "comment": "少しの時間なら待てます",
-  "longitude": "136.881537",
-  "latitude": "35.170915"]
+  "longitude": "136.884556",
+  "latitude": "35.17213"]
   ]
 }

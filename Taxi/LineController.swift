42689926afe59f8a31b05c5bfe8d9bc2f6fa8ac0
Katsuya Taguchi <nymph.y.s.0310@ezweb.ne.jp>
2017-10-25 12:20:35 +0900

    最短経路画面の作成

---
+++ Taxi/LineController.swift
@@ -0,0 +1,28 @@
+//
+//  LineController.swift
+//  Taxi
+//
+//  Created by Katsuya Taguchi on 2017/10/25.
+//  Copyright © 2017年 清野隼史. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LineController: UIViewController {
+    
+    let ud = UserDefaults.standard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var mapLine: MKMapView!
+    @IBAction func backHome(_ sender: UIButton) {
+    }
+    
+}
